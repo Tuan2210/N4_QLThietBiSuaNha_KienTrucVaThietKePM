b616f6ddf164211f35360db79d93fcb6571a4b15
Tuan2210 <quangtuan496>
2022-11-19 21:13:41 +0700

    create eureka sv

---
+++ EurekaServer/src/test/java/com/se/suanha/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.suanha.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
