9540762d52ebb60a38ff2bd4b1e626459f9278e0
Tuan2210 <quangtuan496>
2022-11-25 14:47:18 +0700

    add doc phân công

--- quan-ly-do-xay-nha/src/main/java/com/se/suanha/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.se.suanha.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-	extends AbstractSecurityWebApplicationInitializer {
-
-}
--- quan-ly-do-xay-nha/src/main/webapp/resources/js/before_signup_signin.js
+++
@@ -1,3 +0,0 @@
-// $('#btnDKDN').click(function() {
-//     location.assign("../html/home_after_signup_signin.html");
-// })
